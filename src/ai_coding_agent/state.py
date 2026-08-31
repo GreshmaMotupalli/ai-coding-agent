0f17c4cd4cbf2e3981790e4cd7e2cfa934dd07d2
Greshma-Motupalli <greshmamotupalli9@gmail.com>
2026-08-31 15:24:22 +0530

    Enhance CodingState with iteration field
    Added 'iteration' field to CodingState TypedDict.

--- src/ai_coding_agent/state.py
+++ src/ai_coding_agent/state.py
@@ -1,14 +1,14 @@
-from typing import TypedDict, Annotated
-
-from langchain_core.messages import BaseMessage
+from typing import Annotated
+from typing_extensions import TypedDict
 from langgraph.graph.message import add_messages
+from langchain_core.messages import BaseMessage
 
 
 class CodingState(TypedDict):
     user_request: str
     plan: str
     code: str
-
+    iteration: int
     messages: Annotated[
         list[BaseMessage],
         add_messages
