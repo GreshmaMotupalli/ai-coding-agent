3a2bf6e8e7ca9e4e8f0b756278d27428638c39ce
Greshma-Motupalli <greshmamotupalli9@gmail.com>
2026-08-31 17:06:31 +0530

    Add repair_attempts to CodingState TypedDict
    Added 'repair_attempts' field to CodingState.

--- src/ai_coding_agent/state.py
+++ src/ai_coding_agent/state.py
@@ -1,15 +1,26 @@
 from typing import Annotated
+
 from typing_extensions import TypedDict
+
 from langgraph.graph.message import add_messages
+
 from langchain_core.messages import BaseMessage
 
 class CodingState(TypedDict):
+
     user_request: str
+
     plan: str
+
     code: str
+
     filename: str
+
     test_result: str
+
     iteration: int
+
+    repair_attempts: int
 
     messages: Annotated[
         list[BaseMessage],
